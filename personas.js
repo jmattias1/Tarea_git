33d7ff427103b3fbcf021e805619b28262439e96
Karen Gonzalez <kagoncaoca@gmail.com>
2022-05-27 21:38:29 -0300

    lista de personas

---
+++ personas.js
@@ -0,0 +1,13 @@
+const persona = [{
+    nombre : 'Maria',
+    edad : 24,
+    profesion : "Programadora "
+},
+{
+    nombre : 'Jose',
+    edad : 35,
+    profesion : "Tester QA " 
+}
+]
+
+console.log(persona);
